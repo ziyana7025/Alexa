757e184eb99b707bc48096613c7e9bd4c64e71b7
ziyana7025 <ziyanaaami@gmail.com>
2024-11-21 01:42:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=OFMnERKI#MSAeYuZGA6imejii-N0P_ay57rxHVbzc9fNbKa-e_Ug",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
